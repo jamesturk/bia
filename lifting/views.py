d907467561e9c8d1991510da2520aac49c012132
James Turk <james.p.turk@gmail.com>
2015-04-01 18:19:20 -0400

    basic upload test

--- lifting/views.py
+++ lifting/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django import forms
 
-# Create your views here.
+
+class FitnotesUploadForm(forms.Form):
+    file = forms.FileField()
+
+
+def fitnotes_upload(request):
+    if request.method == 'POST':
+        form = FitnotesUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(request.FILES['file'])
+    else:
+        form = FitnotesUploadForm()
+    return render(request, 'upload.html', {'form': form})
