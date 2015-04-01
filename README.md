a3bf766642621b38607ee27b631dfe75270cd5a8
James Turk <james.p.turk@gmail.com>
2015-04-01 17:02:42 -0400

    housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 personification of force and pure energy
+
+    vagrant up db
+    pip3 install -r requirements.txt
+    ./manage.py migrate
+    ./manage.py runserver
--- web/settings.py
+++ web/settings.py
@@ -5,11 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i-j!g6wtv7^2&gy#45bseqjuoh_yv@=#$6pt0o9%3w#(t=15fd'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -60,8 +56,6 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -75,20 +69,12 @@ DATABASES = {
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
-
 STATIC_URL = '/static/'
