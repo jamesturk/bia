32fac6e442571606bcf55dd6391dd0f25e4b54cb
James Turk <james.p.turk@gmail.com>
2015-04-03 16:46:59 -0400

    initial month view

--- lifting/views.py
+++ lifting/views.py
@@ -1,11 +1,41 @@
+import datetime
+import os
 import tempfile
+import calendar
+from collections import defaultdict
 
 from django.shortcuts import render
 from django import forms
 from django.contrib.auth.decorators import login_required
-import os
+from django.views.generic import dates
 
 from . import importers
+from .models import Set
+
+
+@login_required
+def month(request, year, month):
+    year, month = int(year), int(month)
+
+    sets_by_day = defaultdict(list)
+    for workset in Set.objects.filter(user=request.user, date__year=year, date__month=month):
+        sets_by_day[workset.date.day].append(workset)
+    date = datetime.date(year, month, 1)
+    first_day, max_days = calendar.monthrange(year, month)
+    # make first_day use 0 for sunday
+    first_day = (first_day + 1) % 7
+
+    # start calendar with a few blank days
+    days = [None]*first_day
+
+    for day in range(max_days+1):
+        days.append({'number': day, 'sets': sets_by_day[day]})
+
+    days_by_week = [days[0:7], days[7:14], days[14:21], days[21:28], days[28:35]
+
+    return render(request, 'month.html', { 'date': date,
+                                          'days': days_by_week
+                                          })
 
 
 class FitnotesUploadForm(forms.Form):
--- web/urls.py
+++ web/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^fitnotes-upload/$', 'lifting.views.fitnotes_upload'),
+    url(r'^lifting/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'lifting.views.month'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
