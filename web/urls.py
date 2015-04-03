0a8658a441aa0d1db4d3715d41afd1c74212b559
James Turk <james.p.turk@gmail.com>
2015-04-03 14:02:19 -0400

    bring in a bit of the Scale theme

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^fitnotes-upload/$', 'lifting.views.fitnotes_upload'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
