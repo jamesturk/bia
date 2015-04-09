2a9bf4dc2b28c4d323af6bc7529a14a7077ff201
James Turk <james.p.turk@gmail.com>
2015-04-09 16:58:04 -0400

    start on edit profile view

--- web/urls.py
+++ web/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 
 import lifting.urls
+import profiles.urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^profile/', include(profiles.urls.urlpatterns)),
     url(r'^lifting/', include(lifting.urls.urlpatterns)),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
